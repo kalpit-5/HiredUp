5ccbe723b48b418200246fb3b6e8c5a2687d05f1
kalpit-5 <kalpitparmar55@gmail.com>
2025-01-31 12:53:44 +0530

    Completed Navbar Component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router";
+import Home from "./components/Home";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  },{
+    path:'/signup',
+    element:<Signup/>
+  },
+])
+
+function App() {
+  return <>
+    <RouterProvider router = {appRouter}/>
+  </>;
+}
+
+export default App;
---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,80 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../ui/popover";
+import { LogOut, UserRound } from "lucide-react";
+import React from "react";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+
+
+
+const Navbar = () => {
+  const user = false;
+  return (
+    <div className="bg-white">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16 ">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Hired<span className="text-[#F83002]">Up</span>
+          </h1>
+          <h1>helloooo</h1>
+        </div>
+        <div className="flex items-center gap-16">
+          <ul className=" flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+          </ul>
+
+          {!user ? (
+            <div className="flex items-center gap-2">
+            
+              <Button variant="outline">Login</Button>
+              <Button className="bg-[#6A38c2] hover:bg-[#7f43e8]">
+                Signup
+              </Button>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src="https://shorturl.at/pE7wu" />
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent className="w-80">
+                <div className="">
+                  <div className="flex gap-2 space-y-2">
+                    <Avatar className="cursor-pointer mt-3">
+                      <AvatarImage src="https://shorturl.at/pE7wu" />
+                    </Avatar>
+                    <div>
+                      <h4 className="font-medium"> kalpit parmar</h4>
+                      <p className="text-sm text-gray-500">
+                        Helllo !! my name is kalpit
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-col my-2 text-gray-600">
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                      <UserRound></UserRound>
+                      <Button variant="link">View profile</Button>
+                    </div>
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                      <LogOut></LogOut>
+                      <Button variant="link">Logout</Button>
+                    </div>
+                  </div>
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+
