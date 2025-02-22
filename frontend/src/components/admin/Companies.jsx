7577df67d8ec641db3d260f2c87171e2eed48414
kalpit-5 <kalpitparmar55@gmail.com>
2025-02-22 21:58:35 +0530

    Fixed Login Not Working

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -1,24 +1,38 @@
-import React from "react";
-import Navbar from "../shared/Navbar";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
-import CompaniesTable from "./CompaniesTable";
-import { Navigate, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import Navbar from '../shared/Navbar'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import CompaniesTable from './CompaniesTable'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { setSearchCompanyByText } from '@/redux/companySlice'
+import useGetAlllCompanies from '@/hooks/useGetAlllCompanies'
 
 const Companies = () => {
+    useGetAlllCompanies();
+    const [input, setInput] = useState("");
     const navigate = useNavigate();
-  return (
-    <div>
-      <Navbar />
-      <div className=" max-w-6xl mx-auto my-10">
-        <div className="flex items-center justify-between my-5">
-          <Input className="w-fit" placeholder="Filter by name" />
-          <Button onClick={()=>navigate("/admin/companies/create")}>New Company</Button>
-        </div>
-        <CompaniesTable/>
-      </div>
-    </div>
-  );
-};
+    const dispatch = useDispatch();
 
-export default Companies;
+    useEffect(()=>{
+        dispatch(setSearchCompanyByText(input));
+    },[input]);
+    return (
+        <div>
+            <Navbar />
+            <div className='max-w-6xl mx-auto my-10'>
+                <div className='flex items-center justify-between my-5'>
+                    <Input
+                        className="w-fit"
+                        placeholder="Filter by name"
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <Button onClick={() => navigate("/admin/companies/create")}>New Company</Button>
+                </div>
+                <CompaniesTable/>
+            </div>
+        </div>
+    )
+}
+
+export default Companies
--- frontend/src/redux/companySlice.js
+++ frontend/src/redux/companySlice.js
@@ -1,16 +1,24 @@
+import Companies from "@/components/admin/Companies";
 import { createSlice } from "@reduxjs/toolkit";
 
 const companySlice = createSlice({
   name: "company",
   initialState: {
     singleCompany: null,
+    companies: [],
   },
   reducers: {
     setSingleCompany: (state, action) => {
       state.singleCompany = action.payload;
     },
+    setCompanies: (state, action) => {
+      state.companies = action.payload;
+    },
+    setSearchCompanyByText: (state, action) => {
+      state.searchCompanyByText = action.payload;
+    },
   },
 });
 
-export const { setSingleCompany } = companySlice.actions;
+export const { setSingleCompany, setCompanies,setSearchCompanyByText } = companySlice.actions;
 export default companySlice.reducer;
