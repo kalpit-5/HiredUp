78cac1a8e26ba340191fd7f2579bc727ad0b7b07
kalpit-5 <kalpitparmar55@gmail.com>
2025-05-05 11:28:51 +0530

    Project Completed

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -141,3 +141,37 @@ export const updateStatus = async (req, res) => {
     console.log(error);
   }
 };
+
+
+// export const updateStatus = async (req, res) => {
+//   try {
+//     const { status } = req.body;
+//     const applicationId = req.params.id;
+
+//     if (!status) {
+//       return res.status(404).json({
+//         message: "status is required",
+//         success: false,
+//       });
+//     }
+
+//     const application = await Application.findOne({_id:applicationId});
+
+//     if (!application) {
+//       return res.status(404).json({
+//         message: "application not found",
+//         success: false,
+//       });
+//     }
+    
+//     application.status = status.toLowerCase();
+//     await application.save();
+
+//     return res.status(200).json({
+//       message:"application status changed successfully",
+//       success:true
+//     })
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
